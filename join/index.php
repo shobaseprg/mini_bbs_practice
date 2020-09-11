d3dbd1bbd18c62a6d2f0d07ee894fde70582466d
shobaseprg <pgymlsho19128@gmail.com>
2020-09-11 19:52:15 +0900

    error_message

--- join/index.php
+++ join/index.php
@@ -1,3 +1,12 @@
+<?php
+if ($_POST['name'] === "") {
+	$error['name'] = 'blank';
+}
+if ($_POST['password'] === "") {
+	$error['password'] = 'blank';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,7 +14,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>会員登録</title>
+	<link rel="stylesheet" href="../style.css" />
   <body>
+		<form action="", method="post", nctype="multipart/form-data">
+			<input type='text', name="name", value=""><br>
+				<?php if ($error['name'] === 'blank') {
+					echo '名前いれろ<br>';
+				}
+				?>
+			<input type='password', name='password', value=""><br>
+			<?php if ($error['password'] === 'blank') : ?>
+        パスいれろ<br>
+			<?php endif ?>
+			<input type='submit', value='submit'><br>
+		</form>
 
   </body>
 </html>
